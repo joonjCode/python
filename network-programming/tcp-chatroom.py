217dfc44f0808ad532e2f1e0a591f11dceb5e186
Joon <jtj308@naver.com>
2020-08-12 02:52:10 +0900

    tcp chatroom added

--- network-programming/tcp-chatroom.py
+++ network-programming/tcp-chatroom.py
@@ -6,3 +6,48 @@ port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handel(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except :
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} has left the chat'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+        
+        client.send('Joon'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickanme of the client is {nickname}')
+
+        broadcast(f'{nickname} joined the chat'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=handel, args=(client,))
+        thread.start()
+
+
+print('Server is listening')
+receive()
