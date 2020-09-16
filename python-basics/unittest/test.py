819874f6e7bdc52e542cffe6274b1e2406de3c98
Joon <jtj308@naver.com>
2020-09-17 07:30:52 +0900

    unittest added

---
+++ python-basics/unittest/test.py
@@ -0,0 +1,15 @@
+import unittest
+from main import do_stuff
+
+class TestMain(unittest.TestCase):
+	def test_do_stuff(self):
+		test_param = 10
+		result = do_stuff(test_param)
+		self.assertEqual(result, 15)
+	def test_do_stuff2(self):
+		test_param = 'asdff'
+		result = do_stuff(test_param)
+		self.assertTrue(isinstance(result,ValueError))
+
+
+unittest.main()
