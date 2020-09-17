64fead1f6c3523a819662b8e62237fbb919962b9
Joon <jtj308@naver.com>
2020-09-17 23:19:15 +0900

    random game unittest modified

--- python-basics/unittest/random_game_test.py
+++ python-basics/unittest/random_game_test.py
@@ -8,15 +8,15 @@ class TestGame(unittest.TestCase):
         self.assertTrue(result)
 
     def test_input_wrong_guess(self):
-        result = random_game.run_guess(0, )
+        result = random_game.run_guess(0, 1)
         self.assertFalse(result)
 
     def test_input_wrong_number(self):
-        result = random_game.run_guess(11, 5)
+        result = random_game.run_guess(5,11)
         self.assertFalse(result)
 
     def test_input_wrong_type(self):
-        result = random_game.run_guess('11', 5)
+        result = random_game.run_guess(5, '11')
         self.assertFalse(result)
 
 if __name__ == '__main__':
--- python-basics/unittest/test.py
+++ python-basics/unittest/test.py
@@ -2,14 +2,38 @@ import unittest
 from main import do_stuff
 
 class TestMain(unittest.TestCase):
+
+	def setUp(self):
+		print('about to test a function')
+
 	def test_do_stuff(self):
+		'''First test'''
 		test_param = 10
 		result = do_stuff(test_param)
 		self.assertEqual(result, 15)
+
 	def test_do_stuff2(self):
 		test_param = 'asdff'
 		result = do_stuff(test_param)
 		self.assertTrue(isinstance(result,ValueError))
 
+	def test_do_stuff3(self):
+		test_param = None
+		result = do_stuff(test_param)
+		self.assertEqual(result, 'please enter a number')
+	
+	def test_do_stuff4(self):
+		test_param = ''
+		result = do_stuff(test_param)
+		self.assertEqual(result, 'please enter a number')
+	
+	# Usually for database
+	def tearDown(self):
+		print('clean up')
 
-unittest.main()
+# Run the test not the file itself
+if __name__ == '__main__':
+	unittest.main()
+
+
+# python -m unittest -v(verbose)
