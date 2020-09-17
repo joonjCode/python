9c47b8f04857c68d6ad6c6f8570fae16e84e0131
Joon <jtj308@naver.com>
2020-09-17 23:15:37 +0900

    random game with functions

--- python-basics/random_game.py
+++ python-basics/random_game.py
@@ -1,29 +1,35 @@
 from random import randint
 import sys
-# Generate a number 1 ~ 100
-first_num = int(sys.argv[1])
-second_num = int(sys.argv[2])
-answer = randint(first_num,second_num)
-# randint(1, 100) random int
+def get_random_number():
+    first_num = int(sys.argv[1])
+    second_num = int(sys.argv[2])
+    answer = randint(first_num,second_num)
+    return first_num, second_num, answer
+# print(get_random_number())
 
+def play_game(first_num:int, second_num:int, answer:int):
+    count: int = 0
+    while True:
+        try:
+            # input from the user
+            guess = int(input(f'Guess a number {first_num}-{second_num} : '))
+            # Check if the number is the right guess.
+            if first_num<= guess <= second_num:
+                if guess > answer:
+                    print('Guess lower')
+                    count += 1
+                elif guess < answer:
+                    print('Guess higher')
+                    count +=1
+                elif guess == answer:
+                    print(f'Good job you got it in {count} tries')
+                    break
+            else:
+                # Check if that input is between 1~100
+                print(f'The number is not bwtn {first_num}-{second_num}')
+        except ValueError as e:
+            print('Please enter a number')
+            continue
 
-print(answer)
-while True:
-    try:
-        # input from the user
-        guess = int(input(f'Guess a number {first_num}-{second_num} : '))
-        # Check if the number is the right guess.
-        if first_num< guess < second_num:
-            if guess > answer:
-                print('Guess lower')
-            elif guess < answer:
-                print('Guess higher')
-            elif guess == answer:
-                print('Good job')
-                break
-        else:
-            # Check if that input is between 1~100
-            print(f'The number is not bwtn {first_num}-{second_num}')
-    except ValueError as e:
-        print('Please enter a number')
-        continue
+f,s,a = get_random_number()
+play_game(f,s,a)
