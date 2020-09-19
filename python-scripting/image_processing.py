9f8e08872470015c9f0b2c190e65a63f431eefc7
Joon <jtj308@naver.com>
2020-09-19 16:06:30 +0900

    image-processing with PIL done

--- python-scripting/image_processing.py
+++ python-scripting/image_processing.py
@@ -10,4 +10,14 @@ filtered_img = img.filter(ImageFilter.BLUR)
 
 # L : Gray
 filtered_img = img.convert('L')
-filtered_img.save('./images/gray.png', format='png')
+# crooked = filtered_img.rotate(90)
+
+# resize = filtered_img.resize((300,300))
+# thumbnail 
+# crooked.save('./images/crooked.png', format='png')
+# resize.save('./images/resize.png', format='png')
+
+
+box = (100, 100, 400,400)
+region= filtered_img.crop(box)
+region.save('./images/cropped.png', format='png')
